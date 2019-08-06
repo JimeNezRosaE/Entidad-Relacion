0bbf06f2925e688b3656d69d660f9cf851f54b96
CarlosAngela <angela123>
2019-08-06 18:17:40 -0500

    Proyecto-Entidad

---
+++ Parroquia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Parroquia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rosa Elena Jimenez Gonzalez
+            //Angela Patricia Carlos Siatama
+
+
+
+            Proyecto proyec = new Proyecto();
+            proyec.IngresarDatos();
+            proyec.ImprimirDatos();
+            proyec.ModificarDatos();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Parroquia/Proyecto.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parroquia
+{
+    class Proyecto
+    {
+
+
+        //Rosa Elena Jimenez Gonzalez
+        //Angela Patricia Carlos Siatama
+
+
+
+        #region Atributos
+
+        #endregion
+
+        #region Propiedades
+
+        #endregion
+
+        public int Libro { get; set; }
+        public int Folio { get; set; }
+
+        public string Nombre_Per { get; set; }
+
+
+
+        #region Contructores
+
+        public Proyecto(int libro,int folio,string nombre_per)
+        {
+
+            Libro = libro;
+            Folio = folio;
+            Nombre_Per = nombre_per;
+
+        }
+        public Proyecto()
+        {
+
+
+
+        }
+
+
+        #endregion
+
+
+        #region Metodos
+
+        public void IngresarDatos()
+        {
+            Console.WriteLine("Ingrese el libro: ");
+            Libro = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el folio");
+            Folio = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese nombre de la persona: ");
+            Nombre_Per = Console.ReadLine();
+        }
+
+        public void ImprimirDatos()
+        {
+
+
+            Console.WriteLine($"El libro es: {Libro}");
+            Console.WriteLine($"El folio es: {Folio}");
+            Console.WriteLine($"El nombre es: {Nombre_Per}");
+
+
+        }
+
+        public void ModificarDatos()
+        {
+
+            Console.WriteLine("Ingrese nuevo libro: s/n");
+            string res = Console.ReadLine();
+            if (res == "s")
+            {
+                Console.WriteLine("Digite nuevo libro: ");
+                Libro = int.Parse(Console.ReadLine());
+            }
+
+            else;
+        }
+
+
+        #endregion
+
+    }
+}
